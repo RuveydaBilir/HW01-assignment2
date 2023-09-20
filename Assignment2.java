a48750834e9281bedb24ff2fb0e67c70be2976a4
Hilal COŞGUN <hilal.cosgun@ug.bilkent.edu.tr>
2023-09-20 16:19:36 +0300

    method d is implemented.

--- Assignment2.java
+++ Assignment2.java
@@ -6,6 +6,18 @@ class Assignment2{
   //method c
 
   //method d
+  
+  public static double[] findDifference (int[] arr){
+    int sum = 0;
+    for(int i = 0; i < arr.length; i++){
+      sum += arr[i];
+    }
+    double[] differences = new double[arr.length];
+    for(int i = 0; i < arr.length; i++){
+      differences[i] = arr[i] - sum;
+    }
+    return differences;
+  }
 
   //method e
   
