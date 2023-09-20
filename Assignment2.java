42afcdb4795148d3bb8ce3510f8026185f44b842
İrem Esendemir <irem.esendemir@ug.bilkent.edu.tr>
2023-09-20 16:16:01 +0300

    method c is implemented.

--- Assignment2.java
+++ Assignment2.java
@@ -4,7 +4,25 @@ class Assignment2{
   //method b
 
   //method c
+  public static int findMin(int[] arr){
+    int minNum;
+    minNum = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (minNum < arr[i]) {
+        minNum = arr[i];
+      } 
+    }
+  }
 
+  public static int findMax(int[] arr){
+    int maxNum;
+    maxNum = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (maxNum < arr[i]) {
+        maxNum = arr[i];
+      } 
+    }
+  }
   //method d
 
   //method e
