c83af877864440fea6aca642ae23b583754fc42e
YunusCelik <21yunuscelik21@gmail.com>
2023-09-20 16:17:31 +0300

    Method e

--- Assignment2.java
+++ Assignment2.java
@@ -1,12 +1,16 @@
 class Assignment2{
-  //method a
-
-  //method b
-
-  //method c
-
-  //method d
-
-  //method e
-  
+    public void sumOfOddsAndEvens(int[] list) {
+        int oddSum = 0;
+        int evenSum = 0;
+        for(int i = 0; i < list.length; i++) {
+            if(list[i] % 2 == 1) {
+                oddSum += list[i];
+            }
+            else {
+                evenSum += list[i];
+            }
+        }
+        System.out.println("Sum of the odd numbers is " + oddSum);
+        System.out.println("Sum of the even numbers is " + evenSum);
+    }
 }
