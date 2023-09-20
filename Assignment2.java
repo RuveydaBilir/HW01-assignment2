6434061025e9b5b56fd9e7357bd6c0ad00365795
Hilal COŞGUN <hilal.cosgun@ug.bilkent.edu.tr>
2023-09-20 17:02:16 +0300

    method d and  mistakes are fixed

--- Assignment2.java
+++ Assignment2.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 import java.util.Random;
 
 class Assignment2{
-    }
-
-class Assignment2{
   //method a
   public static void main(String[] args) {
       int arraySize;
@@ -21,17 +18,18 @@ class Assignment2{
         randomNumber = rand.nextInt(101);
         arr[i]=randomNumber;
       }
+      System.out.println(Arrays.toString(arr));
 
       displayMenu(arr);
 
     }
   //method b
-  public static void displaymenu(int[] arr){
+  public static void displayMenu(int[] arr){
     int choice;
     Scanner in = new Scanner(System.in);
 
     do {
-      System.out.printf("1.Find the minimum of the array\n2.Find the maximum of the array\n3.Find the average of the array \n4.Find the sum of elements with odd- and even-numbered indexes \n5.Exit");
+      System.out.printf("1.Find the minimum of the array\n2.Find the maximum of the array\n3.Find the differences from the average of the array \n4.Find the sum of elements with odd and even-numbered indexes \n5.Exit\n");
       System.out.print("Enter your choice: ");
       choice = in.nextInt();
       if ( choice == 1 ){
@@ -43,17 +41,15 @@ class Assignment2{
         
       }
       else if ( choice == 3 ){
-        System.out.printf("differences from average %s%n",Arrays.toString(findDifference(arr)));
+        System.out.printf("differences from average %s%n",Arrays.toString(findDifferences(arr)));
         
       }
       else if ( choice == 4 ){
-        System.out.printf("sum of elements with odd-numbered indexes:%d%n sum of elements with even-numbered indexes:%d",sumOfOddsAndEvens(arr)[0],sumOfOddsAndEvens(arr)[1]);
+        System.out.printf("sum of elements with odd-numbered indexes: %d%nsum of elements with even-numbered indexes: %d%n",sumOfOddsAndEvens(arr)[0],sumOfOddsAndEvens(arr)[1]);
         
       }
       
     }while(choice != 5);
-
-
   }
 
   //method c
@@ -61,10 +57,11 @@ class Assignment2{
     int minNum;
     minNum = arr[0];
     for (int i = 0; i < arr.length; i++) {
-      if (minNum < arr[i]) {
+      if (minNum > arr[i]) {
         minNum = arr[i];
       } 
     }
+    return minNum;
   }
 
   public static int findMax(int[] arr){
@@ -75,12 +72,24 @@ class Assignment2{
         maxNum = arr[i];
       } 
     }
+    return maxNum;
   }
   //method d
+  public static double[] findDifferences(int[] arr){
+    double sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    double[] differences = new double[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      differences[i] = Math.round(sum / arr.length) - arr[i];
+    }
+    return differences;
+  }
 
   //method e
   
-    public void sumOfOddsAndEvens(int[] list) {
+    public static int[] sumOfOddsAndEvens(int[] list) {
         int oddSum = 0;
         int evenSum = 0;
         for(int i = 0; i < list.length; i++) {
@@ -91,7 +100,7 @@ class Assignment2{
                 evenSum += list[i];
             }
         }
-        System.out.println("Sum of the odd numbers is " + oddSum);
-        System.out.println("Sum of the even numbers is " + evenSum);
+        int[] array = {oddSum, evenSum};
+        return array;
     }
 }
